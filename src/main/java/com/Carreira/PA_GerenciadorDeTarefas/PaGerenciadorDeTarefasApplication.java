39f9ec93c563265e5a4825ab0a8f7eadf1887a31
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-20 11:58:01 -0300

    Model

---
+++ src/main/java/com/Carreira/PA_GerenciadorDeTarefas/PaGerenciadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaGerenciadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaGerenciadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Carreira/PA_GerenciadorDeTarefas/PaGerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaGerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
